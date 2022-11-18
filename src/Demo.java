b98b3bdcae6c445ac226442f6270e77fe7a12cd0
QingFeng1412 <105803457+QingFeng1412@users.noreply.github.com>
2022-11-18 19:35:20 +0800

    changes

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("master");
-
+         System.out.println("hahah");
         System.out.println("demo1");
     }
 }
